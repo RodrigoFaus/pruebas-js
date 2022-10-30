e4cfa7e0331fc3742e380dae61242e8f96a0eca5
rfaus <rodridevelop@hotmail.com>
2022-10-30 13:59:12 -0300

    third commit

--- pruebitas.js
+++ pruebitas.js
@@ -1,4 +1,4 @@
-///////////////////////////////////////////////// Array y constructor
+///////////////////////////////////////////////// Array y constructor////////////////////////////////////////////////////
 
 let arraySecciones = []
 let arrayProductos = []
@@ -8,11 +8,13 @@ class Menu {
         this.nombre = nombre
         this.descripcion = descripcion
         this.categoria = categoria
+
     }
 
 }
 
-///////////////////////////////////////////////// Secciones
+///////////////////////////////////////////////// Secciones////////////////////////////////////////////////////
+
 let btnCreaSeccion = document.getElementById("btnCreaSeccion")
 
 btnCreaSeccion.addEventListener("click", () => {
@@ -24,27 +26,94 @@ btnCreaSeccion.addEventListener("click", () => {
 	asignoSecciones()
 })
 
-
+//Funcion que crea las secciones
 function creoSección() {
 	let newLi = document.createElement("li")
+	let newDiv = document.createElement("div")
+	let p = document.createElement("p")
+	let depositoDiv = document.getElementById("depositoProductos")
 	let depositoSecciones = document.getElementById("depositoSecciones")
-		let nameSecciones = document.getElementById("nameSecciones")
+	let nameSecciones = document.getElementById("nameSecciones")
+	let nuevo = document.createElement("p")
 
 
-arraySecciones.forEach(section => {
-	
-	newLi.textContent = section
-	depositoSecciones.appendChild(newLi)
 
-	newLi.addEventListener("click", () =>{	
-		nameSecciones.textContent = section
+	newDiv.classList = "contenedor-prod"
 
+	arraySecciones.forEach(section => {
+		let pruebaFiltrado = arrayProductos.filter((prod) => prod.categoria == section)
+		nuevo.innerHTML = pruebaFiltrado
+
+		newLi.textContent = section
+		newLi.classList = "secciones"
+		
+		depositoSecciones.appendChild(newLi)
+		newLi.setAttribute("data-categorias",section)
+		p.textContent = section
+
+		newDiv.setAttribute("data-categorias",section)
+
+		newDiv.appendChild(p)
+		depositoDiv.appendChild(newDiv)
+		depositoDiv.appendChild(nuevo)
+		
+
+		newLi.addEventListener("click", () =>{	
+			nameSecciones.textContent = section
+
+		})
 	})
 
-})
-	
+	filtraContenidoSeccion()
 	
 }
+
+function filtraContenidoSeccion(){
+	const seccionesCreadas = document.querySelectorAll(".secciones")
+	const contenedorProductosCreados = document.querySelectorAll(".contenedor-prod")
+	let categoriaActiva = null;
+
+	seccionesCreadas.forEach((seccionCreada) => {
+
+		
+		
+
+		seccionCreada.addEventListener("click", (e) => {
+
+			seccionesCreadas.forEach((elemento) => {
+				elemento.classList.remove("active")
+			})
+
+			e.currentTarget.classList.toggle("active")
+			categoriaActiva = seccionCreada.dataset.categorias;
+			console.log(categoriaActiva)
+
+
+			// Se activa el contenedor de productos que corresponde
+
+			contenedorProductosCreados.forEach((contenedor) => {
+
+				if (contenedor.dataset.categorias === categoriaActiva) {
+					contenedor.classList.add("active")
+
+				} else {
+					contenedor.classList.remove("active")
+				}
+
+			})
+
+		})
+	
+	})
+
+
+
+}
+
+
+
+
+//Funcion que crea las seccciones en el select y options del html
 
 
 function asignoSecciones() {
@@ -54,25 +123,26 @@ function asignoSecciones() {
     arraySecciones.forEach(section => {
         option.textContent = section
         select.appendChild(option)
-
-
     })
 
 }
 
-console.log(arraySecciones)
-
+//Funcion que limpia input
 function limpio() {document.getElementById("inputSecciones").value = ""}
 
-/////////////////////////////////////////////////Productos
+/////////////////////////////////////////////////Productos////////////////////////////////////////////////////
 
 
 let btnCreoProducto = document.getElementById("btnCreoProducto")
 
 btnCreoProducto.addEventListener("click", () => {
 	creoProducto();
+	
+
 })
 
+
+//Funcion que crea los productos
 function creoProducto() {
 
 	let inputNombre = document.getElementById("inputNombre").value
@@ -80,18 +150,23 @@ function creoProducto() {
 	let inputCategoria = document.getElementById("inputGroupSelect01").value
 
 	let producto = new Menu(inputNombre,inputDescripcion,inputCategoria)
-
 	arrayProductos.push(producto)
 
 	limpioInputsProd();
-	
+
 
 }
 
+
+//Funcion que limpia input de productos
 function limpioInputsProd () {
 	document.getElementById("inputNombre").value = ""
 	document.getElementById("inputDescripcion").value = ""
 	document.getElementById("inputGroupSelect01").value = ""
+
 }
+
+
+
 
 console.log(arrayProductos)
